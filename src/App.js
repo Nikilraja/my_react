f4bd682a2c532b876838cf64b6d4e4fbb8a0a3c4
Nikilraja <nikilrajbca@gmail.com>
2022-12-08 19:57:07 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          sample react
         </a>
       </header>
     </div>
